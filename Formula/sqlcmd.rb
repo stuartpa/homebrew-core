cddf804f309085da242efef4525bcc7586ba43b4
Stuart Padley <stuartpa@hotmail.com>
2022-09-15 09:38:33 -0700

    Update Formula/sqlcmd.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/sqlcmd.rb
+++ Formula/sqlcmd.rb
@@ -1,9 +1,8 @@
 class Sqlcmd < Formula
   desc "Microsoft SQL Server command-line interface"
   homepage "https://github.com/microsoft/go-sqlcmd"
-  url "https://github.com/microsoft/go-sqlcmd.git",
-  tag:      "v0.8.1",
-  revision: "4e0b95ce49b8164c6496ca1b7a85fa57734fef4c"
+  url "https://github.com/microsoft/go-sqlcmd/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "35a2d165ae6e1f39033ef7ab9ef3b0b205dc1b6b5583912a082297e93ad1c8ab"
   license "MIT"
 
   depends_on "go" => :build
